048d993b8ca11d9c73caa3d466436548c5fa0fc9
Oscar <oscar.of.oz@gmail.com>
2026-06-06 17:27:44 -0700

    FIX: include new pages in build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
-export default defineConfig({})
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        scienceFiction: resolve(__dirname, 'science-fiction.html'),
+        growingUp: resolve(__dirname, 'growing-up.html'),
+      },
+    },
+  },
+})
